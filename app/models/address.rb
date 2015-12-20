41ec1e9e8c46f13aa62dd752655b467609a7992e
Ian Carrico <git@iancarrico.com>
2015-12-20 16:39:57 -0600

    Add in relationships

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,8 @@
 class Address < ActiveRecord::Base
+  belongs_to :household
+
+  # Check to see if an address is international or not.
+  def is_international?
+    internation.present?
+  end
 end
--- app/models/household.rb
+++ app/models/household.rb
@@ -1,2 +1,10 @@
 class Household < ActiveRecord::Base
+  has_many :addresses
+  has_many :people
+  belongs_to :head_of_house, class_name: "Person",
+                     primary_key: :id,
+                     foreign_key: :head_of_house
+
+
+
 end
