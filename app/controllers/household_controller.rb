cd2e175c2df5c5fe9e8b7605343dfb88ac286df4
Ian Carrico <git@iancarrico.com>
2015-12-21 07:31:05 -0600

    Add some base views, routes, and household controller updates

--- app/controllers/household_controller.rb
+++ app/controllers/household_controller.rb
@@ -1,12 +1,15 @@
 class HouseholdController < ApplicationController
   before_action :set_household, only: [:show]
 
+  def index
+    @households = Household.order(updated_at: :desc).paginate(:page => params[:page], :per_page => 25)
+  end
 
 
   private
 
   def set_household
-    @household = Household.includes(:head_of_house, :address).find(params[:id])
+    @household = Household.includes(:head_of_house).includes(:addresses).find(params[:id])
 
   end
 
